ca77e666462011d0961fdf57896a311438bc83af
slavnem <ecevituzmali@gmail.com>
2024-02-25 16:30:11 +0300

    Hashtag (#) bayrağını ile çıktı vermek

---
+++ bolum-9/hashtag-bayrak/hastag-bayrak.c
@@ -0,0 +1,18 @@
+// Slavnem @2024-02-25
+// Hashtag (#) Bayrak
+
+// Kütüphaneler
+#include <stdio.h>
+
+// main
+int main(void)
+{
+    int c = 1427; // c ye başlangıç değeri atanması
+    double p = 1427.0; // p ye başlangıç değeri atanması
+
+    printf("%#o\n", c);
+    printf("%#x\n", c);
+    printf("%#X\n", c);
+    printf("\n%g\n", p);
+    printf("%#g\n", p);
+}
