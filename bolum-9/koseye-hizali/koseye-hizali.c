7dd31dd8654b0627cb2159a45e41e13dad42cf1d
slavnem <ecevituzmali@gmail.com>
2024-02-25 07:15:46 +0300

    Çıktı metinini sağa doğru köşeye hizalama

---
+++ bolum-9/koseye-hizali/koseye-hizali.c
@@ -0,0 +1,21 @@
+// Slavnem @2024-02-25
+// Köşeye Hizalı
+
+// Kütüphaneler
+#include <stdio.h>
+
+// main
+int main(void)
+{
+    printf("%4d\n", 1);
+    printf("%4d\n", 12);
+    printf("%4d\n", 123);
+    printf("%4d\n", 1234);
+    printf("%4d\n\n", 12345);
+
+    printf("%4d\n", -1);
+    printf("%4d\n", -12);
+    printf("%4d\n", -123);
+    printf("%4d\n", -1234);
+    printf("%4d\n", -12345);
+}
