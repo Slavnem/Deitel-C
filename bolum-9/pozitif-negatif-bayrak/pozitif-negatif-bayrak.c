f7fc98f4b7c1c46ac700b495bedb27b1b8cdbbcb
slavnem <ecevituzmali@gmail.com>
2024-02-25 15:57:44 +0300

    Pozitif negatif çıktı bayrakları kullanımı

---
+++ bolum-9/pozitif-negatif-bayrak/pozitif-negatif-bayrak.c
@@ -0,0 +1,12 @@
+// Slavnem @2024-02-25
+// Pozitif Negatif Bayrak
+
+// Kütüphaneler
+#include <stdio.h>
+
+// main
+int main(void)
+{
+    printf("%d\n%d\n", 786, -786);
+    printf("%+d\n%+d\n", 786, -786);
+}
