9004c247d2aa2d7122148e7ffa53f8c2726a2e5b
slavnem <ecevituzmali@gmail.com>
2024-02-25 15:53:36 +0300

    Çıktı verilecekleri sağa ya da sola doğru hizalama

---
+++ bolum-9/saga-sola-hizalama/saga-sola-hizalama.c
@@ -0,0 +1,12 @@
+// Slavnem @2024-02-25
+// Sağa Sola Hizalama
+
+// Kütüphaneler
+#include <stdio.h>
+
+// main
+int main(void)
+{
+    printf("%10s%10d%10c%10f\n", "hello", 7, 'a', 1.23);
+    printf("%-10s%-10d%-10c%-10f\n", "hello", 7, 'a', 1.23);
+}
