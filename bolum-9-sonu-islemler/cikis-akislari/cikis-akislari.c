67a50193e821b959d50765514102bc55b751ae3d
slavnem <ecevituzmali@gmail.com>
2024-02-27 01:23:10 +0300

    Çıkış akış işlemlerini örnek ile kullanmak

---
+++ bolum-9-sonu-islemler/cikis-akislari/cikis-akislari.c
@@ -0,0 +1,19 @@
+// Slavnem @2024-02-27
+// Çıkış Akışları
+
+// Kütüphaneler
+#include <stdio.h>
+
+// main
+int main(void)
+{
+    printf("Test \'cikis akisi 1\'\n"); // \'
+    printf("Test \" cikis akisi 2\"\n"); // \"
+    printf("Test \? cikis akisi 3\?\n"); // \?
+    printf("Test \\ cikis akisi 4\\\n");
+    printf("Test \a cikis akisi 5\a\n"); // \a
+    printf("Test \b cikis akisi 6\b\n"); // \b
+    printf("Test \n cikis akisi 8\n\n"); // \n
+    printf("Test \r cikis akisi 7\r\n"); // \r
+    printf("Test \t cikis akisi 9\t\n"); // \t
+}
