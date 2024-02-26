84d4bf860f5da196b6f89625ab864772ead29f8d
slavnem <ecevituzmali@gmail.com>
2024-02-26 14:20:04 +0300

    Girilen sayının hepsini almak yerine alanlara ayırıp, ilk 2 hane ve son 4 hane ayrı alınması

---
+++ bolum-9/alan-genisligi/alan-genisligi.c
@@ -0,0 +1,17 @@
+// Slavnem @2024-02-25
+// Alan Genişliği
+
+// Kütüphaneler
+#include <stdio.h>
+
+// main
+int main(void)
+{
+    int x;
+    int y;
+
+    printf("%s", "6 haneli tam sayi giriniz: ");
+    scanf("%2d%d", &x, &y);
+
+    printf("Girilen tam sayilar sirayla %d ve %d\n", x, y);
+}
