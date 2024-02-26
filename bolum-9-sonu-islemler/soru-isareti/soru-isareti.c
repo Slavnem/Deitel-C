b87c2b30793b28c97bd9d431a35436f85fa1cb76
slavnem <ecevituzmali@gmail.com>
2024-02-27 02:39:11 +0300

    Belirteç olan \? değil normal ? isareti olarak alma

---
+++ bolum-9-sonu-islemler/soru-isareti/soru-isareti.c
@@ -0,0 +1,14 @@
+// Slavnem @2024-02-27
+// Soru İşareti
+
+// Kütüphaneler
+#include <stdio.h>
+
+// main
+int main(void)
+{
+    char c; // karakter değişkeni oluşturma
+
+    scanf("%[?]", &c); // soru işaretini alma
+    printf("%c\n", c); // çıktı verme
+}
