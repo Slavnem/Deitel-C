62da0601685a0fa7f417022c8026b742553f9a91
slavnem <ecevituzmali@gmail.com>
2024-02-26 11:29:47 +0300

    Tamsayı belirteçleri tamsayıları okuma

---
+++ bolum-9/tamsayilar-okuma/tamsayilar-okuma.c
@@ -0,0 +1,23 @@
+// Slavnem @2024-02-26
+// Tamsayılar Okuma
+
+// Kütüphaneler
+#include <stdio.h>
+
+// main
+int main(void)
+{
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+    int f;
+    int g;
+
+    puts("Yedi tane tam sayi giriniz:");
+    scanf("%d%i%i%i%o%u%x", &a, &b, &c, &d, &e, &f, &g);
+
+    puts("\nOndalik tam sayi olarak goruntulenen girdiler:");
+    printf("%d %d %d %d %d %d %d\n", a, b, c, d, e, f, g);
+}
