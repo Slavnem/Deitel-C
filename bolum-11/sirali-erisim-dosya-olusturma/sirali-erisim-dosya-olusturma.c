bb51eb16adeb6c49841b663dfb3f25682786af3f
slavnem <ecevituzmali@gmail.com>
2024-05-08 19:49:49 +0300

    C de stdio.h kütüphanesinde bulunan FILE ile dosya varsa direk, yoksa oluşturup üstüne kullanıcı dosya sonu işaretçisi girene kadar yeni veri yazmak

---
+++ bolum-11/sirali-erisim-dosya-olusturma/sirali-erisim-dosya-olusturma.c
@@ -0,0 +1,44 @@
+// Slavnem @2024-05-08
+// Sıralı Erişim Dosyası Oluşturma
+
+// Kütüphaneler
+#include <stdio.h>
+
+// Yapı
+typedef struct
+{
+    unsigned int hesap; // hesap numarası
+    char ad[30]; // hesap adı
+    double balance; // hesap bakiyesi
+} Hesap;
+
+// main
+int main(void)
+{
+    FILE *cfPtr; // cfPtr = clients.dat dosyası dosya işaretçisi
+
+    // fopen dosyayı açar. Program dosyasını oluşturamıyorsa
+    if((cfPtr = fopen("clients.dat", "w")) == NULL) {
+        puts("Dosya Acilamadi");
+        return 0; // programı sonlandır
+    }
+
+    Hesap bankaHesap; // banka hesabı
+
+    // Kullanıcıdan bilgileri istemek
+    puts("Hesap, ad ve bakiye giriniz.");
+    puts("Giris islemini sonlandirmak icin EOF giriniz.");
+
+    printf("%s", "? ");
+    scanf("%d%29s%lf", &bankaHesap.hesap, &bankaHesap.ad, &bankaHesap.balance);
+
+    // fprintf ile dosya içine hesap, isim ve bakiye değerini yazmak
+    while(!feof(stdin))
+    {
+        fprintf(cfPtr, "%d %s %.2f\n", bankaHesap.hesap, bankaHesap.ad, bankaHesap.balance);
+        printf("%s", "? ");
+        scanf("%d%29s%lf", &bankaHesap.hesap, &bankaHesap.ad, &bankaHesap.balance);
+    }
+
+    fclose(cfPtr); // fclose dosyayı kapatır
+}
